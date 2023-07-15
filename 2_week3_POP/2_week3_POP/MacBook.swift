3d96827b0541c029d5fc7d1ed3ffe1694ffc2ac9
DasanKim <tempzipp@gmail.com>
2023-07-15 09:29:26 +0900

    MacBook 타입 생성

---
+++ 2_week3_POP/2_week3_POP/MacBook.swift
@@ -0,0 +1,17 @@
+//
+//  MacBook.swift
+//  2_week3_POP
+//
+//  Created by Dasan on 2023/07/15.
+//
+
+struct MacBook {
+    let allowableChargeWattPerHour: WattPerHour
+    let currentBatteryCapacity: WattPerHour
+    let maxBatteryCapacity: WattPerHour = 100
+    
+    func chargeBattery(charger: Chargeable) {
+        // 충전기를 활용해 배터리를 완전히 충전한 뒤 충전에 걸린 시간을 print함
+        
+    }
+}
